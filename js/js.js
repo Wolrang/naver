d0d857d3aebfaffee5b098ae6bbc3836c8950d1a
Wolrang <dkfud252@naver.com>
2023-01-25 15:34:42 +0900

    🎉 네이버 회원가입 폼

--- js/js.js
+++ js/js.js
@@ -3,7 +3,18 @@ $('#main-btn').on('click', function() {
   $('.btn-1').css('display', 'none')
 });
 
+$('#main-btn-2').on('click', function() {
+  $('.btn-2').removeClass('open');
+  $('.btn-1').css('display', 'block')
+});
+
+$('.btn-1').on('click', function() {
+  $('.btn-2').addClass('open');
+  $('.btn-1').css('display', 'none')
+});
+
 $('.btn-2').on('click', function() {
   $('.btn-2').removeClass('open');
   $('.btn-1').css('display', 'block')
 });
+
